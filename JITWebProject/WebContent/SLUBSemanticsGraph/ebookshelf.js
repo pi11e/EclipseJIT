cef981c9dfbb3b6e6699cf04b4cdbe1f1acddbc8
pi11e <darthkad@hotmail.com>
2013-05-29 22:19:35 +0200

    added async queryDB functionality, not working correctly!

--- JITWebProject/WebContent/SLUBSemanticsGraph/ebookshelf.js
+++ JITWebProject/WebContent/SLUBSemanticsGraph/ebookshelf.js
@@ -70,8 +70,28 @@ var promptQueryAndExecute = function()
 	queryDB(queryString, function(data){alert(data);});
 };
 
-var queryDB = function(queryString, callback)
+var queryDB = function(queryString, callback, callAsync)
 {
+	// make sure the supplied callback is a function
+	if(typeof callback !== 'function')
+	{
+		console.log("queryDB ERROR: invalid callback parameter. not a function.");
+		return;
+	}
+	
+	
+	if(arguments.length === 3 && typeof callAsync === 'boolean') // async parameter supplied
+	{
+		// use callAsync parameter to set up jQuery AJAX accordingly
+		jQuery.ajaxSetup({async:callAsync});
+	}
+	else
+	{
+		// default: asynchronous call (true)
+		jQuery.ajaxSetup({async:true});
+	}
+	
+	
 	$.get(
 		    "http://localhost:8080/JITWebProject/DataServlet",
 		    {query : queryString},
@@ -90,7 +110,7 @@ var getDistinctValuesForTag = function(tag, callback)
 	queryDB(query, callback);
 };
 
-var createGraphLevelFromData = function()
+var createFirstGraphLevel = function()
 {
 	getDistinctValuesForTag("a99d3", 
 	function(data) 
@@ -104,12 +124,7 @@ var createGraphLevelFromData = function()
        // TODO: create new json graph data with given names
        
        // 1. create new node for each valid name in nodeNames
-       //		node = {id:"", name:"", data:{...}}
-       //		data has additional values 
-       /*highlightColor: "#F90"
-       isHighlighted: false
-       regularColor: "#278"
-       */
+       
        
        for(var i = 0; i < nodeNames.length; i++)
 	   {
@@ -130,15 +145,21 @@ var createGraphLevelFromData = function()
     				
     		   }; // end of newNode
     		   
-    		   console.log("name result = " + newNode.name);
-  				var query = "XQUERY count(//obj[a99d3='"+ newNode.name +"'])";
-   				//console.log("trying query = " + query);
+
+    			// TODO: add book count for each node?
+  				var query = "XQUERY for $x in 1 return (count(//obj[a99d3='"+ newNode.name +"']), "+ newNode.id +")";
+  				  				
    				queryDB(query, function(data)
    				{
-   					console.log("server response to " + query + " = " + data);
-   					// get correct node
+   					// data is expected to match format $amount_of_objects $node_id
+   					var tempData = data.split(" ");
+   					var childAmount = tempData[0];
+   					var nodeId = tempData[1];
    					
-   				});
+   					// doesn't work. while the correct data.cnt gets set to the correct nodes, even when refreshed/plotted the numbers don't show
+   					window.kinectComponent.getNodeById(nodeId).data.cnt = childAmount;
+   					
+   				}, false);
     		   
     		   var rootNode = rgraph.graph.getNode(rgraph.root);
     		   //console.log("created node " + newNode.name + " with id " + newNode.id);
@@ -147,11 +168,11 @@ var createGraphLevelFromData = function()
     		   
     		   
 		   }
-	   }
+	   } // end of node-creating for-loop
        
-
-
-   	
+       // test
+       createSublevelForNode("Arbeiterfotografie");
+       
        
        // 3. reload graph 
        rgraph.refresh();
@@ -161,8 +182,42 @@ var createGraphLevelFromData = function()
        
     }); // end of getDistinctValuesForTag
 	
-	// add book count for each node?
 	
 	
+}; // end of createFirstGraphLevel()
+
+var createSublevelForNode = function(parentNodeName)
+{
+	// Assume parentNodeName is one of the possible distinct values of tag a99d3, e.g. 'Talleyrand' or 'Arbeiterfotografie'.
+	// For the given name, display a number of subnodes depending on interaction design mode.
+	var query = "XQUERY for $x in //obj[a99d3='"+parentNodeName+"'] return (distinct-values($x//a55df/text()), ';')";
+	var tempData;
+	
+	queryDB(query, function(data)
+				{
+					// data will contain several tags delimited by ';'
+					tempData = data.split(";");
+					console.log(tempData);
+				});
 };
 
+var addNodesWithNames = function(nodeNames)
+{
+	for(var i = 0; i < nodeNames.length; i++)
+	{
+		// create a new node for each given node name
+		var nodeName = nodeNames[i];
+		
+		var newNode = {
+				   id:		Math.ceil(Math.random()*100000).toString(),
+				   name:	jQuery.trim(nodeName),
+				   data:	{
+					   			highlightColor: "#F90",
+					   			isHighlighted: false,
+					   			regularColor: "#278",
+					   			cnt: undefined
+					   		}
+				
+		   }; // end of newNode
+	}
+};
