6ef304a321c91e906376089afb0fd8a371f8a618
pi11e <darthkad@hotmail.com>
2013-06-26 14:45:53 +0200

    added monitor, websocket kinect notifications

--- JITWebProject/WebContent/websocket.js
+++ JITWebProject/WebContent/websocket.js
@@ -1,13 +1,22 @@
+var socket;
+var closeConnection = function(){
+	
+	if(socket)
+		socket.close();
+};
+
+
 window.onload = function () {
     var status = document.getElementById("status");
     var canvas = document.getElementById("canvas");
+    var consoleDiv = document.getElementById("console");
     var context = canvas.getContext("2d");
 
     // add border to canvas
     canvas.style.border = "red 1px solid"
     
-//    context.canvas.width = window.innerWidth;
-//    context.canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth / 2;
+    canvas.height = window.innerHeight / 2;
 
     if (!window.WebSocket) {
         status.innerHTML = "Your browser does not support web sockets!";
@@ -17,7 +26,7 @@ window.onload = function () {
     status.innerHTML = "Connecting to server...";
 
     // Initialize a new web socket.
-    var socket = new WebSocket("ws://localhost:8181/KinectHtml5");
+    socket = new WebSocket("ws://localhost:8181/KinectHtml5");
 
     // Connection established.
     socket.onopen = function () {
@@ -31,29 +40,47 @@ window.onload = function () {
 
     // Receive data FROM the server!
     socket.onmessage = function (evt) {
+    	
+    	
+    	
         status.innerHTML = "Kinect data received.";
 
         // Get the data in JSON format.
         var jsonObject = eval('(' + evt.data + ')');
 
-        context.clearRect(0, 0, canvas.width, canvas.height);
-        context.fillStyle = "#FF0000";
-        context.beginPath();
+        
+        
+        
+        // if the incoming json represents skeleton data, draw it in the canvas
+        if(jsonObject.type === 'JSONSkeletonCollection')
+    	{
+        	context.clearRect(0, 0, canvas.width, canvas.height);
+            context.fillStyle = "#FF0000";
+            context.beginPath();
 
-        // Display the skeleton joints.
-        for (var i = 0; i < jsonObject.skeletons.length; i++) {
-            for (var j = 0; j < jsonObject.skeletons[i].joints.length; j++) {
-                var joint = jsonObject.skeletons[i].joints[j];
+            // Display the skeleton joints.
+            for (var i = 0; i < jsonObject.skeletons.length; i++) {
+                for (var j = 0; j < jsonObject.skeletons[i].joints.length; j++) {
+                    var joint = jsonObject.skeletons[i].joints[j];
 
-                // Draw!!!
-                context.arc(parseFloat(joint.x), parseFloat(joint.y), 10, 0, Math.PI * 2, true);
+                    // Draw!!!
+                    context.arc(parseFloat(joint.x), parseFloat(joint.y), 10, 0, Math.PI * 2, true);
+                }
             }
-        }
 
-        context.closePath();
-        context.fill();
+            context.closePath();
+            context.fill();
+            
+    	}
+        // if the incoming json represents a string message, append it to the console div
+        else 
+    	{
+        	consoleDiv.innerHTML = "<p>server message:</p><p>"+jsonObject+"</p>";
+        	//console.log(jsonObject.message);
+        	//jQuery("#console").append("<p> " + jsonObject + "</p>");
+    	}
 
         // Inform the server about the update.
-        socket.send("Skeleton updated on: " + (new Date()).toDateString() + ", " + (new Date()).toTimeString());
+        //socket.send("Skeleton updated on: " + (new Date()).toDateString() + ", " + (new Date()).toTimeString());
     };
 };
